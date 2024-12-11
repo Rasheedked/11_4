89cda64bc05a0acacbe70c801c5e799b13454889
SultanAref <U21200640@sharjah.ac.ae>
2024-12-12 01:04:48 +0400

    Create wishlist.js

---
+++ wishlist.js
@@ -0,0 +1,8 @@
+// Wishlist Data
+const wishlistData = [
+    { id: 1, name: 'CeraVe Dry Skin', price: 'AED 112.00', image: 'https://static.thcdn.com/images/large/original//productimg/1600/1600/11798696-1235192205505719.jpg' },
+    { id: 2, name: 'Tom Ford Ombre Leather', price: 'AED 680.00', image: 'https://www.sephora.ae/dw/image/v2/BKWK_PRD/on/demandware.static/-/Sites-masterCatalog_Sephora/default/dw0b69f434/images/hi-res/PID_principal/P3426001_principal.jpg?sw=585&sh=585&sm=fit' },
+    { id: 3, name: 'Dior Homme Intense', price: 'AED 420.00', image: 'https://shop-beauty.dior.ae/cdn/shop/products/Y0479201_F077926009_E01_GHC_de_DE_1200x1200.jpg?v=1706880763' },
+    { id: 4, name: 'Into the night', price: 'AED 85.00', image: 'https://www.bathandbodyworks.ae/assets/styles/bbw/image-thumb__288114__product_listing/28003659_1.jpg' },
+    { id: 5, name: 'Al-Jadayel Hair Oil', price: 'AED 49.00', image: 'Al-Jadayel Hair Oil.jpg' },
+];
